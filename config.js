ebab4322cc1d0d37c46aa4687e67d38c05e80c06
nickygunasekara-Teams <nickygunasekara@gmail.com>
2026-06-28 12:28:01 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const MONEY_CONFIG = {
+    firebase: {
+        apiKey: "AIzaSyCtiYc94gsZ1AeHpBerEGgFvHkS5-K2IJI",
+        authDomain: "money-ca279.firebaseapp.com",
+        projectId: "money-ca279",
+        storageBucket: "money-ca279.firebasestorage.app",
+        messagingSenderId: "350489193290",
+        appId: "1:350489193290:web:747f80bbecdc42cda2afb3"
+    },
+    fees: {
+        platformFeeILS: 15.00
+    },
+    marketing: {
+        freeTransferEveryNth: 5,
+        remindCustomerAfterDays: 30
+    }
+};
+
+export default MONEY_CONFIG;
